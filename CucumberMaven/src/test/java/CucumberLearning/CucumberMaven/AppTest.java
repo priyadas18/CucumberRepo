d90ea8ec9e5e1d57133297fa35acb7eab61edad6
priyadas21 <priyadas23791@gmail.com>
2021-05-22 21:04:21 +0530

    My first Cucumber Project with details

---
+++ CucumberMaven/src/test/java/CucumberLearning/CucumberMaven/AppTest.java
@@ -0,0 +1,20 @@
+package CucumberLearning.CucumberMaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
